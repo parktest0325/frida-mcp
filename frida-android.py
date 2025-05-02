6c4eaf88e2c46c36c09ad8d01e92387bdea660e3
dongkim <52898964+parktest0325@users.noreply.github.com>
2025-05-02 12:58:13 +0900

    feat: hook, kill

--- frida-android.py
+++ frida-android.py
@@ -11,10 +11,10 @@ _usb_device = None
 
 @mcp.tool()
 def get_usb_device() -> Any:
-    """frida 와 USB로 연결된 장치의 ID를 찾고 저장한다
+    """frida와 USB로 연결된 장치를 찾고 저장하여 다른 도구에서도 사용할 수 있도록 한다. 
     
     Returns:
-        device ID
+        device
     """
     global _usb_device
     if _usb_device is None:
@@ -24,7 +24,7 @@ def get_usb_device() -> Any:
 
 @mcp.tool()
 def hook_apk(package: str, hook_script: str, duration: float = 1.0) -> Dict[str, Any]:
-    """지정한 앱을 후킹하여 스크립트를 삽입하고 처음부터 실행시킨다
+    """켜져있지 않은 앱을 후킹하고 스크립트를 삽입하여 처음부터 실행시킨다
 
     Args:
         package: 후킹할 패키지 이름
@@ -37,25 +37,30 @@ def hook_apk(package: str, hook_script: str, duration: float = 1.0) -> Dict[str,
             "messages": msgs
         }
     """
-    msg_queue = queue.Queue()
+
+    script_ext = """
+    var _log = console.log;
+
+    console.log = function() {
+        var msg = Array.prototype.slice.call(arguments).join(' ');
+        send({ type: 'log', payload: msg });
+        _log.apply(console, arguments);
+    };
+    """
+    msgs  = []
     try:
         pid = _usb_device.spawn([package])
         session = _usb_device.attach(pid)
-        script = session.create_script(hook_script)
+        script = session.create_script(script_ext + hook_script)
 
         ### need fix
         def on_message(message, data):
-            msg_queue.put(message.get("payload"))
+            msgs.append(message.get("payload"))
         script.on("message", on_message)
 
         script.load()
         _usb_device.resume(pid)
-
-        
         time.sleep(duration)
-        msgs = []
-        while not msg_queue.empty():
-            msgs.append(msg_queue.get_nowait())
 
     except Exception as e:
         return {"error": f"HOOK ERROR: {e}"}
@@ -64,3 +69,22 @@ def hook_apk(package: str, hook_script: str, duration: float = 1.0) -> Dict[str,
         "pid":     pid,
         "messages": msgs
     }
+
+@mcp.tool()
+def kill_process(package: str) -> bool:
+    """패키지 이름으로 앱 검색 후 켜져있다면 종료시킨다.
+    Args:
+        package: 종료할 패키지 이름
+
+    Returns:
+        앱이 켜져있어서 정상 종료됐다면 True, 프로세스가 검색되지 않았다면 False 리턴
+    """
+
+    ret = False
+    processes = _usb_device.enumerate_processes()
+    for proc in processes:
+        if proc.name == package:
+            _usb_device.kill(proc.pid)
+            ret = True
+
+    return ret
